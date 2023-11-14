5c3ebf40be39926a5daed6e6e49a2ac54bd938e3
oooO5435tg <morlean@inbox.ru>
2023-11-14 09:20:26 +0700

    a

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -1,10 +1,7 @@
-from django.urls import include
 from django.urls import path
-from django.views.generic import RedirectView
-from django.conf import settings
-from django.conf.urls.static import static
 
-urlpatterns += [
-    path('catalog/', include('catalog.urls')),
-    path('', RedirectView.as_view(url='catalog/', permanent=True)),
-]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+]
