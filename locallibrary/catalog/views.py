321d00bd2b713f1e0cbd6e310777c990e4b416ed
oooO5435tg <morlean@inbox.ru>
2023-11-14 10:25:01 +0700

    Generic list and detail views

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views import generic
 
 from .models import Book, Author, BookInstance, Genre
 
@@ -24,3 +25,9 @@ def index(request):
 
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
+
+class BookListView(generic.ListView):
+    model = Book
+
+class BookDetailView(generic.DetailView):
+    model = Book
